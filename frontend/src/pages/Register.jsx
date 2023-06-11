fb9d4d27e68e65b4e0f08511e8352542f98ab890
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-06-11 18:47:28 -0300

    style: estilizei página de cadastro

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import styles from "../styles/Register.module.css";
+import Logo from "../assets/Logo.svg"
 
 export default function Register() {
   const [user, setUser] = useState({
     email: '',
     password: ''
   });
-  
+
   const navigate = useNavigate();
 
   const handleChange = (event) => {
@@ -27,38 +29,55 @@ export default function Register() {
         user
       );
 
-      if(request.status === 201) {
+      if (request.status === 201) {
         alert('Cadastro realizado com sucesso');
         setTimeout(navigate('/login'), 4000)
       }
+
     } catch (err) {
       console.log(err)
     }
   }
- 
+
   return (
-    <div>
-      <h4>Cadastrar</h4>
-      <form onSubmit={ handleSubmit }>
-        <label>
-          Email
-            <input 
-              type="email" 
-              name="email"
-              value={ user.email }
-              onChange={ handleChange }
-              />
-        </label>
-        <label>
-          Senha
-            <input 
-              type="password"
-              name="password"
-              value={ user.password } 
-              onChange={ handleChange }
-            />
-        </label>
-        <button>
+    <div className={styles.container}>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
+      <form
+        onSubmit={handleSubmit}
+        className={styles.formContainer}
+      >
+        <img 
+          src={ Logo } 
+          className={styles.logo}
+        />
+        <h4>Cadastrar</h4>
+        <p className={styles.label}>Email *</p>
+        <div className={styles.inputEmail}>
+          <span className="material-symbols-outlined">
+            mail
+          </span>
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+            placeholder="email@email.com"
+          />
+        </div>
+        <p className={styles.label}>Senha *</p>
+        <div className={styles.inputPassword}>
+          <span className="material-symbols-outlined">
+            lock
+          </span>
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+            placeholder="Digite sua senha"
+          />
+        </div>
+        <button className={styles.registerBtn}>
           Criar conta
         </button>
       </form>
