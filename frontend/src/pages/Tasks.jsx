a566344d96f03f084ad75801e41feb50f795ab01
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-06-12 16:39:45 -0300

    feat: adicionei input para adição de novas tasks

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -3,6 +3,38 @@ import axios from "axios";
 
 export default function Tasks() {
   const [tasks, setTasks] = useState([]);
+  const [inputTask, setInputTask] = useState('');
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setInputTask(value);
+  };
+
+  const createTask = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const request = await axios.post(
+        'http://localhost:5000/tasks',
+        {
+          taskName: inputTask
+        },
+        {
+          headers: {
+          'Authorization': token,
+          'Content-Type': 'application/json'
+        }}
+      );
+
+      setTasks([
+        ...tasks,
+        request.data.task
+      ]);
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   useEffect(() => {
     async function getTasks() {
@@ -24,6 +56,12 @@ export default function Tasks() {
 
   return (
     <div>
+      <input 
+        type="text" 
+        value={ inputTask }
+        onChange={ handleInputChange }
+      />
+      <button onClick={ createTask }>Criar</button>
       {tasks.map(({ id, taskName }) => (
         <div key={ id }> 
           <p>{ taskName }</p>
