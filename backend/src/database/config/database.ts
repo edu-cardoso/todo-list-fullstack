ac3505c7eb43bafdb5791e8ffe4b9bafaa7bc66d
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-06-19 02:18:48 -0300

    refactor: passei arquivos de config do sequelize para ts

---
+++ backend/src/database/config/database.ts
@@ -0,0 +1,12 @@
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'senha-mysql',
+  database: process.env.DB_NAME || 'todolist',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'mysql',
+}
+
+export = config;
